24003b473eec292d64547e72e3d5fb89d96ab5a5
Dev11 <dev11.webteamwork.ca>
2025-08-28 03:44:55 +0330

    dashbord

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use MohammadMghi\RouteTracker\Http\Controllers\RouteTrackerDashboardController;
 
-Route::middleware(['web', 'auth'])
-    ->prefix('route-tracker')
+Route::prefix('route-tracker')
     ->group(function () {
         Route::get('/dashboard', [RouteTrackerDashboardController::class, 'index'])
             ->name('route-tracker.dashboard');
